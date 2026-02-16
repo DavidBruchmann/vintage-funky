9654748ecc599667731c005092cc6765677b5205
David Bruchmann <david.bruchmann@gmail.com>
2026-02-16 13:17:10 +0700

    [Task] adjust docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,15 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://davidbruchmann.github.io/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://davidbruchmann.github.io',
   baseUrl: '/vintage-funky/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DavidBruchmann',
+  projectName: 'vintage-funky',
 
   onBrokenLinks: 'throw',
 
